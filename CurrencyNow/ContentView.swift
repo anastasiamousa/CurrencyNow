3fa789857c00209c9db02c8b809018fd1327a6df
anastasiamousa <anastasiamousa34@gmail.com>
2025-05-17 00:43:52 +0300

    Initial Commit

---
+++ CurrencyNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyNow
+//
+//  Created by Anastasia Mousa on 17/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyNow/CurrencyNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyNowApp.swift
+//  CurrencyNow
+//
+//  Created by Anastasia Mousa on 17/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
